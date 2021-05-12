14cddbfd6ea54f294de59bbeecd609b4ddaf1380
quietoptimist <41057483+quietoptimist@users.noreply.github.com>
2021-05-12 20:33:15 +0100

    link added

--- scripts/main.js
+++ scripts/main.js
@@ -14,15 +14,21 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
     let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
 }
+
 if(!localStorage.getItem('name')) {
     setUserName();
-  } else {
+    } else {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
+
 myButton.onclick = function() {
     setUserName();
 }
